f21248dafd1948011281a17ea925bac4c6adb954
Adrian Welcker <ardi@diepixelecke.de>
2022-06-02 18:10:51 +0200

    Initial commit

---
+++ starlane-core/starlane-core.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef SLC_STARLANE_CORE_H
+#define SLC_STARLANE_CORE_H
+
+namespace Starlane {
+	void InitBackend();
+}
+
+#endif  // !SLC_STARLANE_CORE_H
---
+++ starlane-qt/starlane.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <QtWidgets/QApplication>
+
+#include <starlane-core.h>
+
+
+int main(int argc, char **argv) {
+	QApplication app(argc, argv);
+	Starlane::InitBackend();
+	return app.exec();
+}
