1c6432b7a727cb733f5ee47d0ee4eae6bdbf56d9
Adrian Welcker <ardi@diepixelecke.de>
2022-10-07 15:13:07 +0200

    Move text-casing stuff to frontend

--- starlane-core/starlane-core.h
+++ starlane-core/starlane-core.h
@@ -4,17 +4,32 @@
 #define SLC_STARLANE_CORE_H
 
 #include <stdint.h>
+#include <string>
 
+// Interface the frontend needs to implement
 namespace SLFrontend {
 // Show a fatal error message. The frontend should refuse any further input
 // after a fatal error has been issued.
 void FatalError(const char *msg);
 // Send text to the output.
 void OutputText(const char *txt);
-}
 
+// Utility functions that are easy to implement using Qt or Glk library features,
+// but are very hard to do in plain C++, so we ask the frontend to do these things for us.
+namespace Services {
+// make an uppercase version of the string `s`
+std::string StrToUpperCase(const std::string &s);
+// make a lowercase version of the string `s`
+std::string StrToLowerCase(const std::string &s);
+// make a sentence-cased version of the string `s`
+std::string StrToSentenceCase(const std::string &s);
+}  // namespace Services
+
+}  // namespace SLFrontend
+
+
+// Interface provided by starlane-core
 namespace Starlane {
-
 // Frontend capabilities and settings
 struct FECapabilities {
 	// Seed for the random number generator, or zero for a random seed.
